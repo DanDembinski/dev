8c484f886925e82af6550f63864c5aa925730ad6
dan <ICYN30@Gmail.com>
2016-02-27 09:49:29 -0500

    Working on flask tutorial for Microblog

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World!"
+	user = {'nickname': 'Dan'} #Fake user
+	return  render_template('index.html',
+				title='home',
+				user=user)
