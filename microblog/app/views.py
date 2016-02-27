2479ed5ba6a8b392526b04ff8536e7084c25648a
dan <ICYN30@Gmail.com>
2016-02-27 13:44:05 -0500

    update

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,10 +1,28 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {'nickname': 'Dan'} #Fake user
+	posts = [
+		{
+			'author': {'nickname' : 'John'},
+			'body' : 'Beautiful Day out!'
+		},
+		{	'author': {'nickname': 'Dan'},
+			'body' : 'Only four more days!'
+		}
+		]
 	return  render_template('index.html',
 				title='home',
-				user=user)
+				user=user,
+				posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html',
+				title='Sign In',
+				form=form)
