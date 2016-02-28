00f05e86259bdf7e17e804beec931fa3f1a68d5a
dan <ICYN30@Gmail.com>
2016-02-27 21:48:18 -0500

    End of Microblog forms chapter of tutorial.

--- microblog/config.py
+++ microblog/config.py
@@ -1,2 +1,7 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = "password"
+
+OPENID_PROVIDERS= [
+	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+	{'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
